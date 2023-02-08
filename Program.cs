05f291b29195ac98f84eab61376c85d294e43bf0
Jaxa Mokoto <modjadji.a.mokoto@gmail.com>
2023-02-08 08:28:05 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+IConfigurationBuilder builder = new ConfigurationBuilder()
+    //specify default path
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddUserSecrets(typeof(Program).Assembly, optional: true)
+    //add a .json file - configuration source and providers
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+// create a config item
+IConfigurationRoot config = builder.Build();
+
+Console.WriteLine("I think the password is..."+ config["Password"]);
